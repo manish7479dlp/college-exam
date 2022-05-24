2a9933ced8ee9fe7fa9c2938fb9c6883ff1a6057
Manish Kumar <manish7479dlp@gmail.com>
2022-05-24 20:32:29 +0530

    Make changes in mongodb_url

--- DataBaseConfig/DBConfig.js
+++ DataBaseConfig/DBConfig.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const { mongoURL } = require("../config/config.js");
 
-const mongodb_url = mongoURL 
-    || "mongodb+srv://manish:E5o89cwq79rHN4GC@cluster0.0cobzpc.mongodb.net/?retryWrites=true&w=majority";
+const mongodb_url = mongoURL || "mongodb+srv://manish:E5o89cwq79rHN4GC@cluster0.0cobzpc.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(mongodb_url).then(() => {
     console.log("Connection Stablish Sucessfully..");
