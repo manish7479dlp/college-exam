0dc0f3ad44a571c7aa3cdaf03e284ecffbf7b102
Manish Kumar <manish7479dlp@gmail.com>
2022-05-28 18:10:34 +0530

    ADD : svg image is added in heading

--- ui/src/Components/Heading/Header.js
+++ ui/src/Components/Heading/Header.js
@@ -1,10 +1,12 @@
 import "./Header.css"
+import Image from "../../Images/clgNameWhite.svg"
 
 const Header = () =>{
     return (
         <>
            <div className="center headerContainer">
                <h3 className="clgName">Murshidabad College of Engineering and Technology</h3>
+               <img className = "collegeName" src = {Image} alt = "image"/>
            </div>
         </>
     )
