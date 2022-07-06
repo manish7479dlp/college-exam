621dc9312fa65d926b449d56bb682dadf3bff58c
Manish Kumar <manish7479dlp@gmail.com>
2022-07-06 12:29:12 +0530

    ADD: only send the current day exam details feature

--- Routes/ExamDetails/ExamDetailsRoute.js
+++ Routes/ExamDetails/ExamDetailsRoute.js
@@ -2,6 +2,21 @@ const express = require("express");
 const router = express.Router();
 const ExamDetails = require("../../Schema/ExamDetails/ExamDetailsSchema");
 
+//Get Current Date;
+
+const getCurrentDate = () => {
+    const DateObject = new Date();
+    let day = DateObject.getDate();
+    let month = DateObject.getMonth()
+    let year = DateObject.getFullYear();
+    month++;
+    
+    day = (day >= 1 && day <= 9) ? "0" + day : day;
+    month = (month >= 1 && month <= 9) ? "0" + month : month;
+    return (year + "-" + month + "-" + day);
+}
+
+
 router.get("/exam_details", async (req, res) => {
   try {
     const response = await ExamDetails.find();
@@ -16,7 +31,12 @@ router.get("/exam_details/:department", async (req , res) => {
   try {
     // const temp = req.params.semesterNumber;
     const response = await ExamDetails.find(req.params);
-    res.send(response)
+    const CurrentDate = getCurrentDate();
+    const result = response.filter((data) => {
+      return data.examDate === CurrentDate;
+    })
+    console.log(result);
+    res.send(result)
   } catch (error) {
     console.log(error);
     res.send(error);
