c50786223a4db6be67591c13ef6b70939de2f97e
Manish Kumar <manish7479dlp@gmail.com>
2022-07-09 12:28:51 +0530

    ADD: student exam given Authentication

--- Routes/Answer/SecondYear/DataStructureAlgorithmRoutes.js
+++ Routes/Answer/SecondYear/DataStructureAlgorithmRoutes.js
@@ -12,6 +12,18 @@ router.get("/data_structure_and_algorithm_answer", async (req, res) => {
   }
 });
 
+// by the help of this router we check student already given exam or not..
+router.get("/data_structure_and_algorithm_answer/:universityRoll", async (req , res) => {
+  try {
+    const response = await DSA.find(req.params).select({universityRoll});
+    console.log(response);
+    res.send(response);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+})
+
 router.post("/data_structure_and_algorithm_answer", async (req, res) => {
   try {
     const response = await DSA(req.body);
