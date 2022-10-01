6b026311a4be7524382e1d56d0d965f889ecd521
Manish Kumar <manish7479dlp@gmail.com>
2022-10-01 22:46:09 +0530

    Add: student all controller is added

---
+++ controllers/studentController.js
@@ -0,0 +1,145 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const studentModel = require("../models/student/studentModel");
+
+const getParticularStudent = async (req, res) => {
+    try {
+        const universityRoll = req.params.universityRoll;
+
+        if (universityRoll) {
+            const response = await studentModel
+                .find({ universityRoll })
+                .select("-password");
+            if (response.length === 0) {
+                res.status(404).send({
+                    status: false,
+                    message: "Invalid University Roll.",
+                });
+            } else {
+                res.status(200).send({ status: true, response: response });
+            }
+        } else {
+            res.send({
+                status: false,
+                message: "Please Enter University Roll.",
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+};
+
+const registerStudent = async (req, res) => {
+    try {
+        const { semester, universityRoll, name, department } = req.body;
+
+        // password = starting 4 digits of universtiy roll and first name of the student
+        let password =
+            req.body.universityRoll.toString().substring(0, 4) +
+            name.split(" ")[0].toLowerCase();
+
+        // encrypt the password
+        password = await bcrypt.hash(password, 10);
+
+        if (semester && universityRoll && name && department) {
+            const response = new studentModel({
+                semester,
+                universityRoll,
+                name,
+                department,
+                password,
+            });
+            const result = await response.save();
+            console.log(result);
+            res.status(201).send({
+                status: true,
+                message: "Details Inserted Sucessfully",
+            });
+        } else {
+            res.send({ status: false, message: "All fields are required." });
+        }
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+};
+
+const getAllStudentDetails = async (req, res) => {
+    try {
+        const response = await studentModel.find().select("-password");
+        if (response.length === 0) {
+            res.status(200).send({ status: false, message: "No data Found" });
+        } else {
+            res.status(200).send({ status: true, response: response });
+        }
+    } catch (error) {
+        console.log(err);
+        res.send(error);
+    }
+};
+
+const deleteParticularStudent = async (req, res) => {
+    try {
+        const _id = req.params._id;
+        const response = await studentModel.findByIdAndDelete({ _id });
+
+        if (response.length === 0) {
+            res.send({ status: false, messgae: "Invalid Id" });
+        } else {
+            res.send({
+                status: true,
+                message: "Student Details Deleted Sucessfully.",
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+};
+
+const updateParticularStudent = async (req, res) => {
+    try {
+        const { semester, universityRoll, name, department } = req.body;
+        const _id = req.params._id;
+        // password = starting 4 digits of universtiy roll and first name of the student
+        let password =
+            req.body.universityRoll.toString().substring(0, 4) +
+            name.split(" ")[0].toLowerCase();
+
+        // encrypt the password
+        password = await bcrypt.hash(password, 10);
+        const response = await studentModel.findByIdAndUpdate(
+            { _id },
+            {
+                $set: {
+                    semester,
+                    universityRoll,
+                    name,
+                    department,
+                    password
+                },
+            }
+        );
+
+        if (response.length === 0) {
+            res.send({ status: false, messgae: "Invalid Id" });
+        } else {
+            res.send({
+                status: true,
+                message: "Student Details Update Sucessfully.",
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+};
+
+module.exports = {
+    getParticularStudent,
+    registerStudent,
+    getAllStudentDetails,
+    deleteParticularStudent,
+    updateParticularStudent,
+};
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors")
 require('dotenv').config();
 const app = express();
 const dbConfig = require("./config/dbConfig");
-
+const studentController = require("./controllers/studentController")
 const port = process.env.PORT;
 const mongoDbUrl = process.env.MONGODB_URL;
 
@@ -19,6 +19,14 @@ app.use(express.json());
 app.get("/" , (req , res) => {
     res.send("hlw from the other side...");
 })
+
+app.get("/student/:universityRoll", studentController.getParticularStudent)
+app.post("/student", studentController.registerStudent)
+app.get("/student", studentController.getAllStudentDetails)
+app.delete("/student/:_id", studentController.deleteParticularStudent)
+app.patch("/student/:_id", studentController.updateParticularStudent)
+
+
 
 app.listen(port , () => {
     console.log("Server is running...");
