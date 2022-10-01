4e475db26b92f10d4abf7cb805e5902be3bfa298
Manish Kumar <manish7479dlp@gmail.com>
2022-10-01 20:43:47 +0530

    Add: student Model is added

---
+++ models/studentModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const StudentSchema = new mongoose.Schema({
+    semester: {
+      type: Number,
+      required: true
+    },
+    universityRoll : {
+        type: Number,
+        unique: true,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    department: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        trim: true
+        // minlength: 8
+    }
+
+});
+
+module.exports = new mongoose.model("studentDetails", StudentSchema);
