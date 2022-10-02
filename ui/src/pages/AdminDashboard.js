bbc8b7a7c9408ee53197a8666af40a99969f742e
Manish Kumar <manish7479dlp@gmail.com>
2022-10-02 16:05:46 +0530

    Add: Implement edit and delete details featue

--- ui/src/pages/AdminDashboard.js
+++ ui/src/pages/AdminDashboard.js
@@ -2,9 +2,53 @@ import react, { useState } from "react";
 import "./AdminDashboardStyle.css";
 import StudentRegistration from "../Components/Registration/StudentRegistration";
 import TeacherRegistration from "../Components/Registration/TeacherRegistration";
+import { toast } from "react-toastify";
 
 const AdminDashboard = () => {
+    const url = "http://localhost:8000/api/student";
+
     const [activeTab, setActiveTab] = useState(1);
+    const [studentData, setStudentData] = useState([]);
+
+    const editStudentDetails = () => {
+         alert("This feature is not implemented yet. (just delete the details and register again)")
+    }
+
+    const deleteStudentDetails = async (id , year) => {
+        const deleteUrl = url + `/${id}`
+        const verify = window.confirm("Do you Really Want to delete the student Details.");
+        if(verify) {
+             const response = await fetch(deleteUrl , {
+                method: "DELETE"
+             })
+
+             const result = await response.json();
+
+             if(result.status) {
+                toast.success(result.message);
+             } else {
+                toast.error(result.message
+                )
+             }
+        } else {
+
+        }
+    }
+
+    const fetchStudentData = async (year) => {
+        try {
+            const response = await fetch(url);
+            const result = await response.json();
+
+            const data = result.response.filter((elm) => {
+                return elm.year === year - 2;
+            });
+
+            setStudentData(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     const studentRegistration = () => {
         setActiveTab(1);
@@ -16,18 +60,22 @@ const AdminDashboard = () => {
 
     const showFirstYearStudentDetails = () => {
         setActiveTab(3);
+        fetchStudentData(3);
     };
 
     const showSecondYearStudentDetails = () => {
         setActiveTab(4);
+        fetchStudentData(4);
     };
 
     const showThirdYearStudentDetails = () => {
         setActiveTab(5);
+        fetchStudentData(5);
     };
 
     const showFourthYearStudentDetails = () => {
         setActiveTab(6);
+        fetchStudentData(6);
     };
 
     const showStudentMarks = () => {
@@ -111,7 +159,43 @@ const AdminDashboard = () => {
                 {/* teacher registration */}
                 {activeTab === 2 && <TeacherRegistration />}
 
-                
+                {/* first year student details */}
+
+                { (activeTab === 3 || activeTab === 4 || activeTab === 5 || activeTab === 6) &&
+                    <div className="studentDetailsShowContainer">
+
+                        {studentData.map((data, idx) => {
+                            return (
+                                <div
+                                    key={idx}
+                                    className="eachStudentDetailSContainer"
+                                >
+                                    <p>
+                                        UniversityRoll: <span>{data.universityRoll}</span>
+                                    </p>
+                                    <p>
+                                        Name: <span>{data.name}</span>
+                                    </p>
+                                    <p>
+                                        Department: <span>{data.department}</span>
+                                    </p>
+                                    <p>
+                                        Year: <span>{data.year}</span>
+                                    </p>
+
+                                    <div className="operationButtonContainer">
+                                        <button className="editButton" onClick={() => {editStudentDetails(data._id)}}>
+                                            Edit
+                                        </button>
+                                        <button className="deleteButton" onClick={() => {deleteStudentDetails(data._id , data.year)}}>
+                                            delete
+                                        </button>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                }
             </div>
         </div>
     );
