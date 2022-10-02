c26a8881446d1d7af78cfce75aeb8b302bf633ff
Manish Kumar <manish7479dlp@gmail.com>
2022-10-02 09:47:32 +0530

    Update: adminController adminLogin section

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,5 @@
 const bcrypt = require("bcrypt");
-const adminModel = require("../models/admin/adminModel")
-
+const adminModel = require("../models/admin/adminModel");
 
 // get all admin
 const getAllAdminDetails = async (req, res) => {
@@ -36,67 +35,85 @@ const getParticularAdmin = async (req, res) => {
 const registerAdmin = async (req, res) => {
     try {
         const { userId, password } = req.body;
-        const hashPassword = await bcrypt.hash(password , 10);
+        const hashPassword = await bcrypt.hash(password, 10);
         const response = new adminModel({
             userId,
-            password: hashPassword
+            password: hashPassword,
         });
         const result = await response.save();
-        res.status(201).send({status: true , message: "Data Inserted Sucessfully"})
+        res.status(201).send({
+            status: true,
+            message: "Data Inserted Sucessfully",
+        });
     } catch (error) {
         res.status(404).send(error);
     }
 };
 
 //update admin details on the basis of id.
-const updateParticularAdmin = async (req , res) => {
-    try {
-         const id = req.params._id;
-         const {userId , password} = req.body;
-         const hashPassword = await bcrypt.hash(password , 10);
-
-         await adminModel.findByIdAndUpdate({_id:id},{$set: {
-            userId , 
-            password: hashPassword
-         }})
-
-         res.send({status: true, message: "Details Updated Sucessfully"})
-
-    } catch (error) {
-        res.send(error)
-    }
-}
-
-//delete particular admin on the basis of id..
-const deleteParticularAdmin = async (req , res) => {
+const updateParticularAdmin = async (req, res) => {
     try {
         const id = req.params._id;
-        await adminModel.findByIdAndDelete({_id:id})
-        res.send({status: true , message: "Data Deleted Sucessfully"})
+        const { userId, password } = req.body;
+        const hashPassword = await bcrypt.hash(password, 10);
+
+        await adminModel.findByIdAndUpdate(
+            { _id: id },
+            {
+                $set: {
+                    userId,
+                    password: hashPassword,
+                },
+            }
+        );
+
+        res.send({ status: true, message: "Details Updated Sucessfully" });
     } catch (error) {
         res.send(error);
     }
-}
+};
+
+//delete particular admin on the basis of id..
+const deleteParticularAdmin = async (req, res) => {
+    try {
+        const id = req.params._id;
+        await adminModel.findByIdAndDelete({ _id: id });
+        res.send({ status: true, message: "Data Deleted Sucessfully" });
+    } catch (error) {
+        res.send(error);
+    }
+};
 
 //admin login
 const adminLogIn = async (req, res) => {
     try {
-        const {userId , password} = req.body;
+        const { userId, password } = req.body;
 
-        if(userId && password) {
-          const response = await adminModel.findOne({userId}).select((-password));
-          if(response) {
-            res.send({status: true, response: response})
-          } else {
-            res.send({status: false , message: "Invalid Details"})
-          }
+        if (userId && password) {
+            const response = await adminModel.findOne({ userId });
+            if (response) {
+                const isMatch = await bcrypt.compare(
+                    password,
+                    response.password
+                );
+                if (isMatch) {
+                    res.send({ status: true, response: response });
+                } else {
+                    res.send({
+                        status: false,
+                        message: "Invalid User Details",
+                    });
+                }
+            } else {
+                res.send({ status: false, message: "Invalid Details" });
+            }
         } else {
-            res.send({status: false , message: "Please Fill all Details"})
+            res.send({ status: false, message: "Please Fill all Details" });
         }
     } catch (error) {
         res.send(error);
     }
-}
+};
 
 module.exports = {
     getAllAdminDetails,
@@ -104,5 +121,5 @@ module.exports = {
     deleteParticularAdmin,
     updateParticularAdmin,
     registerAdmin,
-    adminLogIn
+    adminLogIn,
 };
