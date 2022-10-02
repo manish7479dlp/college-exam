b372fb2bd46f4778e10add1fd1e9fd107d799651
Manish Kumar <manish7479dlp@gmail.com>
2022-10-02 14:49:24 +0530

    Remove extra spaces

--- ui/src/pages/AdminDashboard.js
+++ ui/src/pages/AdminDashboard.js
@@ -6,47 +6,37 @@ import TeacherRegistration from "../Components/Registration/TeacherRegistration"
 const AdminDashboard = () => {
     const [activeTab, setActiveTab] = useState(1);
 
-
-
     const studentRegistration = () => {
-        setActiveTab(1)
-
-    }
+        setActiveTab(1);
+    };
 
     const teacherRegistration = () => {
-        setActiveTab(2)
-    }
+        setActiveTab(2);
+    };
 
     const showFirstYearStudentDetails = () => {
-        setActiveTab(3)
-
-    }
+        setActiveTab(3);
+    };
 
     const showSecondYearStudentDetails = () => {
-        setActiveTab(4)
-
-    }
+        setActiveTab(4);
+    };
 
     const showThirdYearStudentDetails = () => {
-        setActiveTab(5)
-
-    }
+        setActiveTab(5);
+    };
 
     const showFourthYearStudentDetails = () => {
-        setActiveTab(6)
-
-    }
+        setActiveTab(6);
+    };
 
     const showStudentMarks = () => {
-        setActiveTab(7)
-
-    }
+        setActiveTab(7);
+    };
 
     const logoutAdmin = () => {
-        setActiveTab(8)
-
-    }
-    
+        setActiveTab(8);
+    };
 
     return (
         <div className="adminDashboardContainer">
@@ -58,33 +48,68 @@ const AdminDashboard = () => {
 
             <div className="adminDashboardMainContantContainer">
                 <div className="adminNavbar">
-                    <p className= {activeTab === 1 ? "activeTab" : ""} onClick={studentRegistration}>Student Registration</p>
+                    <p
+                        className={activeTab === 1 ? "activeTab" : ""}
+                        onClick={studentRegistration}
+                    >
+                        Student Registration
+                    </p>
 
-                    <p className= {activeTab === 2 ? "activeTab" : ""} onClick={teacherRegistration}>Teacher Registration</p>
+                    <p
+                        className={activeTab === 2 ? "activeTab" : ""}
+                        onClick={teacherRegistration}
+                    >
+                        Teacher Registration
+                    </p>
 
-                    <p className= {activeTab === 3 ? "activeTab" : ""} onClick={showFirstYearStudentDetails}>First Year Student</p>
+                    <p
+                        className={activeTab === 3 ? "activeTab" : ""}
+                        onClick={showFirstYearStudentDetails}
+                    >
+                        First Year Student
+                    </p>
 
-                    <p className= {activeTab === 4 ? "activeTab" : ""} onClick={showSecondYearStudentDetails}>Second Year Student</p>
+                    <p
+                        className={activeTab === 4 ? "activeTab" : ""}
+                        onClick={showSecondYearStudentDetails}
+                    >
+                        Second Year Student
+                    </p>
 
-                    <p className= {activeTab === 5 ? "activeTab" : ""} onClick={showThirdYearStudentDetails}>Third Year Student</p>
+                    <p
+                        className={activeTab === 5 ? "activeTab" : ""}
+                        onClick={showThirdYearStudentDetails}
+                    >
+                        Third Year Student
+                    </p>
 
-                    <p className= {activeTab === 6 ? "activeTab" : ""} onClick={showFourthYearStudentDetails}>Fourth Year Student</p>
+                    <p
+                        className={activeTab === 6 ? "activeTab" : ""}
+                        onClick={showFourthYearStudentDetails}
+                    >
+                        Fourth Year Student
+                    </p>
 
-                    <p className= {activeTab === 7 ? "activeTab" : ""} onClick={showStudentMarks}>Student Marks</p>
+                    <p
+                        className={activeTab === 7 ? "activeTab" : ""}
+                        onClick={showStudentMarks}
+                    >
+                        Student Marks
+                    </p>
 
-                    <p className= {activeTab === 8 ? "activeTab" : ""} onClick={logoutAdmin}>Logout</p>
-
+                    <p
+                        className={activeTab === 8 ? "activeTab" : ""}
+                        onClick={logoutAdmin}
+                    >
+                        Logout
+                    </p>
                 </div>
 
                 {/* student registration */}
-                {
-                    activeTab === 1 && <StudentRegistration/>
-                }
+                {activeTab === 1 && <StudentRegistration />}
 
                 {/* teacher registration */}
-                {
-                    activeTab === 2 && <TeacherRegistration/>
-                }
+                {activeTab === 2 && <TeacherRegistration />}
 
                 
             </div>
