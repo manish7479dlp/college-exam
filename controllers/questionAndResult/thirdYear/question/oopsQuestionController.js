6a5019c3b62f4f4dc95a40634a7232a031d5423b
Manish Kumar <manish7479dlp@gmail.com>
2022-10-04 10:32:55 +0530

    oops question controller code is implemented

---
+++ controllers/questionAndResult/thirdYear/question/oopsQuestionController.js
@@ -0,0 +1,140 @@
+const mongoose = require("mongoose");
+const oopsModel = require("../../../../models/questionAndAnswer/thirdYear/question/oops");
+
+
+
+//Insert new Question
+const registerQuestion = async (req, res) => {
+    try {
+        const { question, opt1, opt2, opt3 , opt4 , answer } = req.body;
+
+        const validationCheck = await oopsModel.findOne({ question });
+
+        if (validationCheck) {
+            res.send({
+                status: false,
+                message: "This question is already Present.",
+            });
+        } else {
+            
+
+            if (question && opt1 && opt2 && opt3 && opt4 && answer ) {
+                const response = new oopsModel({
+                    question,
+                    opt1,
+                    opt2,
+                    opt3,
+                    opt4,
+                    answer
+                });
+                const result = await response.save();
+                res.status(201).send({
+                    status: true,
+                    message: "Question Added Sucessfully",
+                });
+            } else {
+                res.send({
+                    status: false,
+                    message: "All fields are required.",
+                });
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+};
+
+//get All question
+const getAllQuestion = async (req, res) => {
+    try {
+        const response = await oopsModel.find();
+        if (response.length === 0) {
+            res.status(200).send({ status: false, message: "No data Found" });
+        } else {
+            res.status(200).send({ status: true, response: response });
+        }
+    } catch (error) {
+        console.log(err);
+        res.send(error);
+    }
+};
+
+//delete particular question on the basis of id
+const deleteParticularQuestion = async (req, res) => {
+    try {
+        const _id = req.params._id;
+        const response = await oopsModel.findByIdAndDelete({ _id });
+
+        if (response.length === 0) {
+            res.send({ status: false, messgae: "Invalid Id" });
+        } else {
+            res.send({
+                status: true,
+                message: "Question Deleted Sucessfully.",
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+};
+
+//update particular question on the basis of id
+const updateParticularQuestion = async (req, res) => {
+    try {
+        const { question, opt1, opt2, opt3 , opt4 , answer } = req.body;
+        const _id = req.params._id;
+       
+        const response = await oopsModel.findByIdAndUpdate(
+            { _id },
+            {
+                $set: {
+                    question,
+                    opt1,
+                    opt2,
+                    opt3,
+                    opt4,
+                    answer
+                },
+            }
+        );
+
+        if (response.length === 0) {
+            res.send({ status: false, messgae: "Invalid Id" });
+        } else {
+            res.send({
+                status: true,
+                message: "Question Updated Sucessfully.",
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+};
+
+//delete all question
+const deleteAllQuestion =  async(req , res) => {
+    try {
+      const response = await oopsModel.deleteMany({department : "CSE"})
+      
+      if(response) {
+        res.send({status: true , message: "All Question Deleted Sucessfylly."})
+      } else {
+        res.send({status: false , message: "Question is not Delted due to some Technical issues."})
+      }
+    } catch (error) {
+      console.log(error);
+      res.send(error);
+      
+    }
+  }
+
+module.exports = {
+    registerQuestion,
+    getAllQuestion,
+    deleteParticularQuestion,
+    updateParticularQuestion,
+    deleteAllQuestion,
+};
