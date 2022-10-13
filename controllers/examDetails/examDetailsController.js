e262689d49e4e5a49d1a8cfb7d5b8d276ab4ff97
Manish Kumar <manish7479dlp@gmail.com>
2022-10-13 13:01:46 +0530

    start exam time validation check code is implemented

--- controllers/examDetails/examDetailsController.js
+++ controllers/examDetails/examDetailsController.js
@@ -164,21 +164,47 @@ const calExamEndtime = (examStartTime, examDuration) => {
     return `${hour}:${minute}`;
 };
 
+// return value is -ve means exam end or positive means either exam is happending and over if return value is greater than examDuration means exam over
+const isValidTimeForExamination = (examStartTime, examDuration) => {
+    const currTime = getCurrentTime();
+    let currTimeHour = currTime.substring(0, 2);
+    currTimeHour = parseInt(currTimeHour);
+    let currTimeMinutes = currTime.substring(3, 5);
+    currTimeMinutes = parseInt(currTimeMinutes);
+
+    const examEndTime = calExamEndtime(examStartTime, examDuration);
+    let examEndTimeHour = examEndTime.substring(0, 2);
+    examEndTimeHour = parseInt(examEndTimeHour);
+    let examEndTimeMinutes = examEndTime.substring(3, 5);
+    examEndTimeMinutes = parseInt(examEndTimeMinutes);
+
+    // console.log("Current Time = " + currTime);
+    // console.log("End Time = " + examEndTime);
+
+    let timeLeft = (examEndTimeHour - currTimeHour) * 60;
+    timeLeft = timeLeft + examEndTimeMinutes - currTimeMinutes;
+
+    return timeLeft;
+};
+
+// console.log(isValidTimeForExamination("11:50", "30"))
 
 const mayStartExam = async (req, res) => {
     try {
         const _id = req.params._id;
-        const nowTime = getCurrentTime();
         const response = await examDetailsModel.findById({ _id });
-        const examEndTime = calExamEndtime(
+
+        const examStartConfirmation = isValidTimeForExamination(
             response.examStartTime,
             response.examDuration
         );
 
-        if (response.examStartTime <= nowTime && nowTime >= examEndTime) {
-            res.send({ status: true });
-        } else {
+        if (examStartConfirmation < 0) {
+            res.send({ status: false , message: "Exam is Over." });
+        } else if(examStartConfirmation > response.examDuration){
             res.send({ status: false, message: "Exam is not Started" });
+        } else {
+            res.send({status: true})
         }
     } catch (error) {
         res.send(error);
