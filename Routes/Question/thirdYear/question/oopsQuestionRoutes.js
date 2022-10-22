777052dc29ed7cd44398bd23c246ffba97ae4519
Manish Kumar <manish7479dlp@gmail.com>
2022-10-22 12:16:28 +0530

    name change

--- Routes/Question/thirdYear/question/oopsQuestionRoutes.js
+++ Routes/Question/thirdYear/question/oopsQuestionRoutes.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const oopsQuestionController = require("../../../../controllers/questionAndResult/thirdYear/question/oopsQuestionController")
 
 
-router.post("/oops-question", oopsQuestionController.registerQuestion)
-router.get("/oops-question", oopsQuestionController.getAllQuestion)
-router.delete("/oops-question/:_id", oopsQuestionController.deleteParticularQuestion)
-router.patch("/oops-question/:_id", oopsQuestionController.updateParticularQuestion)
-router.delete("/oops-quesiton" , oopsQuestionController.deleteAllQuestion)
+router.post("/object-oriented-programming-question", oopsQuestionController.registerQuestion)
+router.get("/object-oriented-programming-question", oopsQuestionController.getAllQuestion)
+router.delete("/object-oriented-programming-question/:_id", oopsQuestionController.deleteParticularQuestion)
+router.patch("/object-oriented-programming-question/:_id", oopsQuestionController.updateParticularQuestion)
+router.delete("/object-oriented-programming-quesiton" , oopsQuestionController.deleteAllQuestion)
 
 
 module.exports = router;
